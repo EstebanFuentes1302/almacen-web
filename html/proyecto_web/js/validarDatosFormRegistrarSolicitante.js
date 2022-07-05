56b39fa78e517b1a7cba5fe71c517d022b82846b
esteban <esteban.fuentes.1302@gmail.com>
2022-07-04 22:37:29 -0500

    función Registrar Solicitante de php a Javascript + validación de datos y extensión de archivo

---
+++ html/proyecto_web/js/validarDatosFormRegistrarSolicitante.js
@@ -0,0 +1,153 @@
+const formRegistrarSolicitante = document.getElementById('#formRegistrarSolicitante')
+const formRegistrarSolicitanteInputs = document.querySelectorAll('#formRegistrarSolicitante input');
+
+const expresiones = {
+    nombre: /^(([ \u00c0-\u00ffa-zA-Z'\-])+){3,}$/,
+    form_nombre: /^((\w|[ \u0021-\u002f]|[\u00c0-\u00ff])+){2,}/,
+    form_cantidad: /^\d+$/,
+    celular: /9\d{2}[\s-]?\d{3}[\s-]?\d{3}/,
+    correo: /^[\w-._]+@(\w+\.[a-zA-Z]+){1,2}$/,
+    codigo_solicitante: /^[1-9]\d{7}$/,
+    codigo: /^(?!1000)[1-9][0-9][0-9][0-9]$/,
+    usuario: /^\w+(\S?\w+?)+$/,
+    password: /^(([\w!@#\.])+){4,}$/,
+    foto: /^.+\.(jpeg||png||jpg)$/
+}
+
+const campos = {
+    nombre: false,
+    correo: false,
+    telefono: false,
+    foto: false
+}
+
+const validarFormRegistrarSolicitante = (e) => {
+    switch(e.target.name){
+        case "txtNombreSolicitante":
+            if(expresiones.form_nombre.test(e.target.value)){
+                document.getElementById('txtNombreSolicitante').classList.remove('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorNombre').classList.remove('txtErrorShow');
+                campos['nombre']=true;
+            }else{
+                document.getElementById('txtNombreSolicitante').classList.add('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorNombre').classList.add('txtErrorShow');
+                campos['nombre']=false;
+            }
+        break;
+        case "txtCorreo":
+            if(expresiones.correo.test(e.target.value)){
+                document.getElementById('txtCorreo').classList.remove('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorCorreo').classList.remove('txtErrorShow');
+                campos['correo']=true;
+            }else{
+                document.getElementById('txtCorreo').classList.add('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorCorreo').classList.add('txtErrorShow');
+                campos['correo']=false;
+            }
+        break;
+        case "txtTelefono":
+            if(expresiones.celular.test(e.target.value)){
+                document.getElementById('txtTelefono').classList.remove('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorTelefono').classList.remove('txtErrorShow');
+                campos['telefono']=true;
+            }else{
+                document.getElementById('txtTelefono').classList.add('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorTelefono').classList.add('txtErrorShow');
+                campos['telefono']=false;
+            }
+        break;
+        case "foto":
+            console.log(e.target.value);
+            if(expresiones.foto.test(e.target.value)){
+                document.getElementById('foto').classList.remove('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorFoto').classList.remove('txtErrorShow');
+                campos['foto']=true;
+            }else{
+                document.getElementById('foto').classList.add('txtFieldFormIncorrecto');
+                document.getElementById('txtErrorFoto').classList.add('txtErrorShow');
+                campos['foto']=false;
+            }
+        break;
+    }
+}
+
+formRegistrarSolicitanteInputs.forEach((input)=>{
+    input.addEventListener('keyup',validarFormRegistrarSolicitante);
+    input.addEventListener('blur',validarFormRegistrarSolicitante);
+})
+
+
+$('#formRegistrarSolicitante').submit(function(e){
+    e.preventDefault();
+    //console.log(date);
+    //var file_data = $('#foto').prop('files')[0];   
+    var formData = new FormData(document.getElementById('formRegistrarSolicitante'));
+    //foto.append('foto', file_data);
+    var nombre = $('#txtNombreArticulo').val();
+    var correo = $('#txtCorreo').val();
+    var telefono = $('#txtTelefono').val();
+    
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
+    }
+    if(campos['foto']){
+        if(campos['nombre'] && campos['correo'] && campos['telefono']){
+            $.ajax({
+                url: '../controlador/CtrlRegistrarSolicitante.php',
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function (response){
+                    console.log(response);
+                    if(JSON.parse(response)=='true'){
+                        Swal.fire({
+                                title: 'Solicitante Registrado!',
+                                text: 'El solicitante ha sido registrado correctamente',
+                                icon: 'success',
+                                background: '#121212',
+                                color: 'white'
+                            });
+                        $('#formRegistrarArticulo').trigger('reset');
+                    }else if(JSON.parse(response)=='false'){
+                        Swal.fire({
+                        title: 'Error',
+                        text: 'No se pudo registrar Solicitante',
+                        icon: 'error',
+                        background: '#121212',
+                        color: 'white'
+                        })
+                    }else if(JSON.parse(response) == 'noupload'){
+                        Swal.fire({
+                        title: 'Warning',
+                        text: 'No se puede subir la foto',
+                        icon: 'error',
+                        background: '#121212',
+                        color: 'white'
+                        })
+                        $('#formRegistrarArticulo').trigger('reset');
+                    }
+                }
+            });  
+        }else{
+            Swal.fire({
+                    title: 'Error',
+                    text: 'Los datos ingresados no son correctos',
+                    icon: 'error',
+                    background: '#121212',
+                    color: 'white'
+            })
+        }    
+    }else{
+        Swal.fire({
+                    title: 'Error',
+                    text: 'El formato de imagen es incorrecto',
+                    icon: 'error',
+                    background: '#121212',
+                    color: 'white'
+            })
+    }
+    
+})
+
+
--- html/proyecto_web/vista/FrmRegistrarSolicitante.php
+++ html/proyecto_web/vista/FrmRegistrarSolicitante.php
@@ -9,25 +9,40 @@
         <title>Registrar Solicitante</title>
         </head>
         <body>
-            <a class="logout" href="../controlador/CtrlLogout.php">Cerrar Sesión</a>
+            <div class="topPanel">
+                <a class="back" href="../controlador/CtrlShowMenuSolicitante.php">&lt; Volver al Menú</a>
+                <a class="logout" href="../controlador/CtrlLogout.php">Cerrar Sesión</a>
+            </div>
             <h1 align="center">Registrar Solicitante</h1>
-            <form method="post" action="../controlador/CtrlRegistrarSolicitante.php" enctype="multipart/form-data"><table width="652" border="0" align="center" >
+            <form method="post" id="formRegistrarSolicitante" enctype="multipart/form-data"><table width="652" border="0" align="center" >
           <tbody>
             <tr>
               <td width="184" height="41" align="center" valign="middle" class="txtForm">Nombre</td>
-              <td width="458" align="center" valign="middle"><input class="txtFieldForm" type="text" name="txtNombre" id="txtNombre"></td>
+              <td width="458" align="center" valign="middle"><input class="txtFieldForm" type="text" name="txtNombreSolicitante" id="txtNombreSolicitante"></td>
               </tr>
+          <tr>
+              <td height="0" colspan="2" align="center" valign="middle"><p class="txtError" id="txtErrorNombre">El nombre del solicitante debe contener al menos 2 dígitos</p></td>
+            </tr>
             <tr>
               <td class="txtForm" height="32" align="center" valign="middle">Correo Electrónico</td>
               <td align="center" valign="middle"><input class="txtFieldForm" type="email" name="txtCorreo" id="txtCorreo"></td>
+            </tr>
+            <tr>
+              <td height="0" colspan="2" align="center" valign="middle"><p class="txtError" id="txtErrorCorreo">El formato del correo es incorrecto (ejemplo: alonso.peves@hot.com)</p></td>
             </tr>
             <tr>
               <td class="txtForm"  height="47" align="center" valign="middle">Teléfono</td>
               <td align="center" valign="middle"><input class="txtFieldForm" type="text" name="txtTelefono" id="txtTelefono"></td>
             </tr>
             <tr>
+              <td height="0" colspan="2" align="center" valign="middle"><p class="txtError" id="txtErrorTelefono">El número de celular debe contener 9 dígitos y empezar con 9</p></td>
+            </tr>
+            <tr>
               <td class="txtForm"  height="42" align="center" valign="middle">Foto</td>
               <td align="center" valign="middle"><input class="fileForm" type="file" name="foto" id="foto"></td>
+            </tr>
+            <tr>
+              <td height="0" colspan="2" align="center" valign="middle"><p class="txtError" id="txtErrorFoto">La foto debe ser en formato jpg, jpeg o png</p></td>
             </tr>
             <tr>
               <td height="47" colspan="2" align="center" valign="middle">
@@ -38,7 +53,9 @@
             </tbody>
         </table>
         </form>
-            <a class="back" href="../controlador/CtrlShowMenuSolicitante.php">Volver al Menú</a>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+            <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+            <script src="../js/validarDatosFormRegistrarSolicitante.js"></script>
         </body>
         </html>
         <?php
